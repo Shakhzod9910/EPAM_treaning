03502bf552a9e97b89e43fed11216b952b0ad56e
Shakhzodbek <b1901275@mdis.uz>
2022-12-07 15:12:33 +0500

    Practice with data types

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+const string = "Hello world"
+const boolean = true
+const number = 10
+
+
+// Perform addition of various types
+
+console.log(string + boolean); //Hello worldtrue
+console.log(string + number);  //Hello world10
+console.log(number + boolean); //11
+
+// Perform multiplication of various types
+
+console.log(string * boolean); //NaN
+console.log(string * number);  //NaN
+console.log(number * boolean); //10
+
+// Divide different types
+
+console.log(string / boolean); //NaN
+console.log(string / number);  //NaN
+console.log(number / boolean); //10
+
+// Perform explicit conversion
+
+console.log(Number("10"))           // 10
+console.log(Number(true))           // 1
+console.log(String(10))             // "10"
+console.log(String(true))           //"true"
+console.log(Boolean("Hello World")) //true
+console.log(Boolean(10))            //true
